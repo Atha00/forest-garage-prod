4e177b6eecdf760791fd35e8d501a6cdb53f31ed
Michelot Alexis <amichelot@club-internet.fr>
2022-05-31 15:39:43 +0200

    fixing bug

--- models/cars.js
+++ models/cars.js
@@ -11,7 +11,7 @@ module.exports = (mongoose, Mongoose) => {
       year: Number,
       color: String,
       image: {
-        type: Schema.Types.Mixed,
+        type: schema.Types.Mixed,
         default: {},
       },
     },
