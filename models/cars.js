1b8f260cae49df0352eed12f5b4519ff5eadb457
Michelot Alexis <amichelot@club-internet.fr>
2022-05-31 14:49:23 +0200

    added fields inside Model

--- models/cars.js
+++ models/cars.js
@@ -4,13 +4,18 @@
 module.exports = (mongoose, Mongoose) => {
   // This section contains the properties of your model, mapped to your collection's properties.
   // Learn more here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/models/enrich-your-models#declaring-a-new-field-in-a-model
-  const schema = Mongoose.Schema({
-    'model': String,
-    'year': Number,
-    'color': String,
-  }, {
-    timestamps: false,
-  });
+  const schema = Mongoose.Schema(
+    {
+      brand: String,
+      model: String,
+      year: Number,
+      color: String,
+      image: { type: mongoose.Schema.Types.Mixed, default: {} },
+    },
+    {
+      timestamps: false,
+    }
+  );
 
-  return mongoose.model('cars', schema, 'cars');
+  return mongoose.model("cars", schema, "cars");
 };
